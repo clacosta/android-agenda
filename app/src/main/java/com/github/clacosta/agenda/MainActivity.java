07148e6879d0938e06f99da6bd55bf77b834ed4d
Claudio Costa <claudio@imaginaction.com.br>
2019-10-01 16:45:17 -0300

    feet: 01 - Implementando o layout da Activity

--- app/src/main/java/com/github/clacosta/agenda/MainActivity.java
+++ app/src/main/java/com/github/clacosta/agenda/MainActivity.java
@@ -3,12 +3,13 @@ package com.github.clacosta.agenda;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Toast.makeText(this, "Claudio Costa", Toast.LENGTH_LONG).show();
+        setContentView(R.layout.activity_main);
     }
 }
