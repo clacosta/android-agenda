43b9d337f1975e903cef5d02e1a1ce140fa1d164
Claudio Costa <claudio@imaginaction.com.br>
2019-10-01 17:09:09 -0300

    feet: 03 - Iniciando a implementação de layout de lista

--- app/src/main/java/com/github/clacosta/agenda/MainActivity.java
+++ app/src/main/java/com/github/clacosta/agenda/MainActivity.java
@@ -4,12 +4,22 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        List<String> alunos = new ArrayList<>(Arrays.asList("Claudio", "Cica", "Gabi"));
+        TextView primeiroAluno = findViewById(R.id.textView1);
+        TextView segundoAluno = findViewById(R.id.textView2);
+        TextView terceiroAluno = findViewById(R.id.textView3);
+        primeiroAluno.setText(alunos.get(0));
+        segundoAluno.setText(alunos.get(1));
+        terceiroAluno.setText(alunos.get(2));
     }
 }
